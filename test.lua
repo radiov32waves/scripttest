df39bcbd12171256b81939a11439fcd0cc7a797c
radiov32waves <7li.7hmed0@gmail.com>
2026-05-19 20:43:16 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -10,24 +10,20 @@ function LoadScript()
         ConfigFolder = "LucasHubConfig"
     })
 
-    local testTab = Window:MakeTab({Name = "test", Icon = "rbxassetid:/4483345998"})
-    testTab:AddSection({Name = "test"})
+    local slimeTab = Window:MakeTab({Name = "test", Icon = "rbxassetid:/4483345998"})
+    slimeTab:AddSection({Name = "test"})
+    slimeTab:AddButton({Name = "test1", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/dontasklol/Stealingurbrainrots/refs/heads/main/Kurd%20hub"))() end})
+        slimeTab:AddButton({Name = "test2", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53325754de16c11fbf8bf78101c1c881.lua", true))() end})
+            slimeTab:AddButton({Name = "test3", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/469095d9d52d9e123298c64aeb62005f.lua"))() end})
+                slimeTab:AddButton({Name = "test4", Callback = function() loadstring(game:HttpGet("https://rifton.top/loader.lua"))() end})
+                    slimeTab:AddButton({Name = "test5", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6d08fbf253529a4fefa32ff404bd5448.lua"))() end})
+                        slimeTab:AddButton({Name = "test6", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d8824b23a4d9f2e0d62b4e69397d206b.lua"))() end})
+                            slimeTab:AddButton({Name = "test7", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/KaspikScriptsRb/steal-a-brainrot/refs/heads/main/.lua"))() end})
+                                slimeTab:AddButton({Name = "test8", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7d8a2a1a9a562a403b52532e58a14065.lua"))() end})
+                                    slimeTab:AddButton({Name = "test9", Callback = function() loadstring(game:HttpGet(“https://raw.githubusercontent.com/egor2078f/lurkhackv4/refs/heads/main/main.lua”, true))() end})
+                                        slimeTab:AddButton({Name = "test10", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Akbar123s/Script-Roblox-/refs/heads/main/Script%20Brainrot%20New"))() end})
 
-    testTab:AddButton({Name = "test1", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/JNHHGaming/Slime-Rng/refs/heads/main/JN%20HH%20Gaming"))() end})
 
-    testTab:AddButton({Name = "test2", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/kaisenlmao/loader/refs/heads/main/chiyo.lua"))() end})
-
-    testTab:AddButton({Name = "test3", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))() end})
-
-    testTab:AddButton({Name = "test4", Callback = function() loadstring(game:HttpGet("https://rifton.top/loader.lua"))() end})
-
-    testTab:AddButton({Name = "test5", Callback = function() loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/475911550cec4d6d"))() end})
-
-    testTab:AddButton({Name = "test6", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-Infinity-X/refs/heads/main/main.lua'))()"))() end})
-
-    testTab:AddButton({Name = "test7", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/mentayuu/script/refs/heads/main/script.lua"))() end})
-
-    testTab:AddButton({Name = "test8 no", Callback = function() loadstring(game:HttpGet(""))() end})
 
     Nova:Init()
 end
